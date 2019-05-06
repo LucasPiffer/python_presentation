d7bf842229974697857ef91a3fd886e3e28d2e32
LucasPiffer <piffer.lucas@gmail.com>
2019-05-05 21:19:29 -0300

    Numbers and basic operations

--- numbers.py
+++ numbers.py
@@ -1,1 +1,10 @@
 age = 28
+my_float_number = 0.4 + .34
+negative_float_number = -23.4
+
+print("Division", age / my_float_number)
+print("Sum", age + my_float_number)
+print("Multiplication", age * 2)
+print("Exponent", 2 ** 2)
+print("Modulus operator", 20 % 2, "When remainder is not 0", 10 % 3)
+print("Floor division", age // 3)
